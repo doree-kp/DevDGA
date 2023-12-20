406448a854a426756a14838f6692947a535f4b0b
Doréane <kpelengadoreane@gmail.com>
2023-12-20 16:09:26 +0000

    first commit

---
+++ src/main/java/doree/devg/DevGApplication.java
@@ -0,0 +1,13 @@
+package doree.devg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevGApplication.class, args);
+	}
+
+}
